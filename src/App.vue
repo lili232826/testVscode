fc356443cbde217d4eb9fb948cbbc0910a8aad76
qll <lili232826@163.com>
2018-09-17 15:34:55 +0800

    布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,64 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="app-header">
+      <div class="title">后台管理系统</div>
+    </div>
+    <div class="app-content">
+      <div class="side-left">
+        <app-nav></app-nav>
+      </div>
+      <div class="content-right">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AppNav from "@/components/common/AppNav";
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    AppNav
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  height: 100%;
+ flex-flow: column;
+ overflow: hidden;
+}
+.app-header {
+  padding:0 20px;
+  color: #fff;
+  font-size: 24px;
+  flex: 0 0 60px;
+  height: 60px;
+  background-color:#2c3e50;
+  line-height: 60px;
+}
+.app-content{
+flex:1;
+ display: flex;
+    flex-flow: row;
+    .side-left{
+      flex:0 0 300px;
+    }
+    .content-right{
+      flex: 1;
+      padding:10px 20px
+    }
 }
 </style>
